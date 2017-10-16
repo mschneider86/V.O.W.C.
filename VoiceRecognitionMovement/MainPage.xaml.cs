b8cc80711e0956218739a801a1540fb3b0045c01
Matheus Schneider <matheus.ssm@gmail.com>
2017-10-15 23:17:54 -0200

    Inclusão de setas de comando, alterações diversas na interface

--- VoiceRecognitionMovement/MainPage.xaml.cs
+++ VoiceRecognitionMovement/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
@@ -8,6 +9,8 @@ using Windows.Storage;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -31,6 +34,16 @@ namespace VoiceRecognitionMovement
         private const string DEVICE_ESQEUERDA = "ESQUERDA";
         private const string DEVICE_DIREITA = "DIREITA";
 
+        private const string PATH_RIGHT_ARROW_GREEN = "Assets/arrow-right-green.png";
+        private const string PATH_RIGHT_ARROW_RED = "Assets/arrow-right-red.png";
+        private const string PATH_LEFT_ARROW_GREEN = "Assets/arrow-left-green.png";
+        private const string PATH_LEFT_ARROW_RED = "Assets/arrow-left-red.png";
+        private const string PATH_UP_ARROW_GREEN = "Assets/arrow-up-green.png";
+        private const string PATH_UP_ARROW_RED = "Assets/arrow-up-red.png";
+        private const string PATH_DOWN_ARROW_GREEN = "Assets/arrow-down-green.png";
+        private const string PATH_DOWN_ARROW_RED = "Assets/arrow-down-red.png";
+
+
         private SpeechRecognizer recognizer;
 
         Thickness margin;
@@ -39,6 +52,8 @@ namespace VoiceRecognitionMovement
         {
             this.InitializeComponent();
 
+            var appView = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+            appView.Title = "V.O.W.C.";
 
             InicializaVoz();
 
@@ -84,12 +99,22 @@ namespace VoiceRecognitionMovement
         /// </summary>
         /// <param name="session"></param>
         /// <param name="args"></param>
-        private void RecognizerResultGenerated(SpeechContinuousRecognitionSession session, SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        private async void RecognizerResultGenerated(SpeechContinuousRecognitionSession session, SpeechContinuousRecognitionResultGeneratedEventArgs args)
         {
             int count = args.Result.SemanticInterpretation.Properties.Count;
             String cmd = args.Result.SemanticInterpretation.Properties.ContainsKey(TAG_CMD) ? args.Result.SemanticInterpretation.Properties[TAG_CMD][0].ToString() : "";
             String device = args.Result.SemanticInterpretation.Properties.ContainsKey(TAG_DEVICE) ? args.Result.SemanticInterpretation.Properties[TAG_DEVICE][0].ToString() : "";
-            Debug.WriteLine("fala reconhecid: " + cmd);
+
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                Thickness margin = imgCadeira.Margin;
+                if (cmd != string.Empty && device != string.Empty)
+                    txbCommandRecognized.Text = cmd + " " + device;
+                else
+                    txbCommandRecognized.Text = "Comando não reconhecido!";
+            });
+
             switch (cmd)
             {
                 case CMD_ANDAR:
@@ -129,6 +154,10 @@ namespace VoiceRecognitionMovement
                     //TODO implementar parada
                     break;
 
+                default:
+                    txbCommandRecognized.Text = "Comando não reconhecido!";
+                    break;
+
             }
 
 
@@ -166,30 +195,34 @@ namespace VoiceRecognitionMovement
         {
             await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
             () =>
-            {
-                // Your UI update code goes here!
+            {                
                 Thickness margin = imgCadeira.Margin;
 
-
+                
                 switch (direction)
                 {
                     case DEVICE_FRENTE:
                         margin.Left += 10;
+                        ChangeArrowColor(DEVICE_FRENTE);                        
                         break;
 
                     case DEVICE_TRAS:
                         margin.Right += 10;
+                        ChangeArrowColor(DEVICE_TRAS);
                         break;
 
                     case DEVICE_DIREITA:
                         margin.Top += 10;
+                        ChangeArrowColor(DEVICE_DIREITA);
                         break;
 
                     case DEVICE_ESQEUERDA:
                         margin.Top -= 10;
+                        ChangeArrowColor(DEVICE_ESQEUERDA);
                         break;
                 }
 
+                txbDirectionMoved.Text = direction;
                 imgCadeira.Margin = margin;
             }
             );
@@ -197,24 +230,107 @@ namespace VoiceRecognitionMovement
         }
 
         private void btnFrente_Click(object sender, RoutedEventArgs e)
-        {
+        {            
             Move(DEVICE_FRENTE, 1);
+            txbCommandRecognized.Text = "TOQUE SETA DIREITA";
         }
 
         private void btnTras_Click(object sender, RoutedEventArgs e)
         {
             Move(DEVICE_TRAS, 1);
+            txbCommandRecognized.Text = "TOQUE SETA ESQUERDA";
         }
 
         private void btnEsquerda_Click(object sender, RoutedEventArgs e)
         {
             Move(DEVICE_ESQEUERDA, 1);
+            txbCommandRecognized.Text = "TOQUE SETA CIMA";
         }
 
         private void btnDireita_Click(object sender, RoutedEventArgs e)
         {
             Move(DEVICE_DIREITA, 1);
+            txbCommandRecognized.Text = "TOQUE SETA BAIXO";
         }
+
+        /// <summary>
+        /// Método para alterar a cor das setas de acordo com a direção informada
+        /// </summary>
+        /// <param name="direction">Direção em que a cadeira deve se mover</param>
+        private void ChangeArrowColor(string direction)
+        {            
+            List<UIElement> elementsRight = FindChildren(btnFrente);
+            Image imageRight = ((Image)elementsRight[0]);            
+
+            List<UIElement> elementsLeft = FindChildren(btnTras);
+            Image imageLeft = ((Image)elementsLeft[0]);            
+
+            List<UIElement> elementsUp = FindChildren(btnEsquerda);
+            Image imageUp = ((Image)elementsUp[0]);            
+
+            List<UIElement> elementsDown = FindChildren(btnDireita);
+            Image imageDown = ((Image)elementsDown[0]);            
+
+            //Verifica qual a direção foi informada e muda a seta daquela diração para VERDE e as demais para VERMELHO
+            switch (direction)
+            {
+                case DEVICE_FRENTE:
+                    imageRight.Source = new BitmapImage(new Uri(this.BaseUri, PATH_RIGHT_ARROW_GREEN));
+                    imageLeft.Source = new BitmapImage(new Uri(this.BaseUri, PATH_LEFT_ARROW_RED));
+                    imageUp.Source = new BitmapImage(new Uri(this.BaseUri, PATH_UP_ARROW_RED));
+                    imageDown.Source = new BitmapImage(new Uri(this.BaseUri, PATH_DOWN_ARROW_RED));
+                    break;
+
+                case DEVICE_TRAS:
+                    imageRight.Source = new BitmapImage(new Uri(this.BaseUri, PATH_RIGHT_ARROW_RED));
+                    imageLeft.Source = new BitmapImage(new Uri(this.BaseUri, PATH_LEFT_ARROW_GREEN));
+                    imageUp.Source = new BitmapImage(new Uri(this.BaseUri, PATH_UP_ARROW_RED));
+                    imageDown.Source = new BitmapImage(new Uri(this.BaseUri, PATH_DOWN_ARROW_RED));
+                    break;
+
+                case DEVICE_DIREITA:
+                    imageRight.Source = new BitmapImage(new Uri(this.BaseUri, PATH_RIGHT_ARROW_RED));
+                    imageLeft.Source = new BitmapImage(new Uri(this.BaseUri, PATH_LEFT_ARROW_RED));
+                    imageUp.Source = new BitmapImage(new Uri(this.BaseUri, PATH_UP_ARROW_RED));
+                    imageDown.Source = new BitmapImage(new Uri(this.BaseUri, PATH_DOWN_ARROW_GREEN));
+                    break;
+
+                case DEVICE_ESQEUERDA:
+                    imageRight.Source = new BitmapImage(new Uri(this.BaseUri, PATH_RIGHT_ARROW_RED));
+                    imageLeft.Source = new BitmapImage(new Uri(this.BaseUri, PATH_LEFT_ARROW_RED));
+                    imageUp.Source = new BitmapImage(new Uri(this.BaseUri, PATH_UP_ARROW_GREEN));
+                    imageDown.Source = new BitmapImage(new Uri(this.BaseUri, PATH_DOWN_ARROW_RED));
+                    break;
+                //caso a direção informada não corresponda a nenhuma das padrões, muda todas as setas de volta para VERMELHO
+                default:
+                    imageRight.Source = new BitmapImage(new Uri(this.BaseUri, PATH_RIGHT_ARROW_RED));
+                    imageLeft.Source = new BitmapImage(new Uri(this.BaseUri, PATH_LEFT_ARROW_RED));
+                    imageUp.Source = new BitmapImage(new Uri(this.BaseUri, PATH_UP_ARROW_RED));
+                    imageDown.Source = new BitmapImage(new Uri(this.BaseUri, PATH_DOWN_ARROW_RED));
+                    break;
+            }            
+
+        }
+
+        /// <summary>
+        /// Método para percorrer um objeto e retornar todos os seus filhos
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <returns></returns>
+        public List<UIElement> FindChildren(DependencyObject parent)
+        {
+            var _List = new List<UIElement>();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var _Child = VisualTreeHelper.GetChild(parent, i);
+                if (_Child is UIElement)
+                    _List.Add(_Child as UIElement);
+                _List.AddRange(FindChildren(_Child));
+            }
+            return _List;
+        }
+
+
     }
 
 }
